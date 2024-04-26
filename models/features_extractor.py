5824e770f738d23621292c59a96aaddec6f760a6
transieu102 <trantran102206@gmail.com>
2024-04-26 14:50:39 +0700

    'update_load_clip'

--- models/features_extractor.py
+++ models/features_extractor.py
@@ -2,14 +2,16 @@ import torch
 import clip
 from PIL import Image
 import cv2
-def load_clip_model(model="ViT-L/14"):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def load_clip_model(model="ViT-L/14", device = "cuda"):
     model, preprocess = clip.load(model, device=device)
     return model, preprocess
 def extract_image_features(model, preprocess, images, device = "cuda"):
     features = []
     for i in range(len(images)):
-        features.append(model.encode_image(preprocess(Image.fromarray(cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB)))).unsqueeze(0).to(device)).cpu().detach().numpy()
+        image = cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        image = preprocess(image).unsqueeze(0).to(device)
+        features.append(model.encode_image(image)).cpu().detach().numpy()
     return features
 
 def extract_query_features(model, query, device = "cuda"):
